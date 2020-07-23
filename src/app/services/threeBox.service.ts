9c990a48d7c01d40e6c951362638bd82c2ebe8fa
dipaksavaliya <dipak241@gmail.com>
2020-07-23 14:29:05 +0530

    fix 3box issue

--- src/app/services/threeBox.service.ts
+++ src/app/services/threeBox.service.ts
@@ -1,26 +1,57 @@
 import { Inject, Injectable } from '@angular/core';
-import { BoxOptions, GetProfileOptions, Threebox } from './3box';
+import Web3 from 'web3';
+import { BehaviorSubject } from 'rxjs';
 import * as Box from '3box';
-const { profileGraphQL, getProfile, getProfiles, getVerifiedAccounts } = require('3box/lib/api')
+import { BoxOptions, GetProfileOptions, Threebox } from './3box';
+import { AppSettings } from '../config/app.config';
+import { async } from '@angular/core/testing';
+import { id } from 'ethers/utils';
 
 declare let window: any;
 declare let require: any;
 
 @Injectable({ providedIn: 'root' })
 export class ThreeBoxService {
+  // box: any;
+  space: any;
+  thread: any;
+  posts: any;
+  user: any;
+
+  private _box = new BehaviorSubject<Threebox>(null);
+  public box$ = this._box.asObservable();
 
   constructor() {
     (async () => {
-      // await window.ethereum.enable()
-      // const profile = await getProfile(window.web3.eth.coinbase)
-      // console.log("profile", profile);
+      const profile = await Box.getProfile(window.web3.eth.coinbase)
+      console.log("profile", profile);
     })();
   }
 
+  public get box(): Threebox {
+    return this._box.getValue();
+  }
+
+  public set box(box: Threebox) {
+    this._box.next(box);
+  }
+
+  // public openBox(address?: string, options?: BoxOptions): Promise<Threebox> {
+  //   return ThreeboxFactory.openBox(address, window.web3.currentProvider, options
+  //   ).then(box => {
+  //     this.box = box;
+  //     return box;
+  //   });
+  // }
+
+  // public getProfile(address: string, options?: GetProfileOptions): Promise<Object> {
+  //   return ThreeboxFactory.getProfile(address, options);
+  // }
+
   async getUserProfile(address: string, options?: GetProfileOptions) {
-    console.log("address", address);
-    // await window.ethereum.enable()
-    // const profile = await Box.getProfile(window.web3.eth.coinbase)
+    await window.ethereum.enable()
+    const profile = await Box.getProfile(window.web3.eth.coinbase)
+    console.log("profile", profile);
 
     const query = `{ 
       profile(id: "${address}" ) {
@@ -32,12 +63,34 @@ export class ThreeBoxService {
     }`
 
     try {
-      let res = await profileGraphQL(query);
-      console.log("res", res.profile);
+      let res = await Box.profileGraphQL(query)
       return res.profile;
     } catch (e) {
       return [];
     }
+  }
+
+  async getAppsThread() {
+    // this.box = await Box.openBox(window.web3.eth.coinbase, window.ethereum);
+    // console.log("box", this.box);
+
+    // this.space = await this.box.openSpace('grant-platform');
+    // console.log("space", this.space);
+
+    // this.thread = await this.space.joinThread("my-grant");
+    // console.log("thread", this.thread);
+
+    // if (!this.thread) {
+    //   console.error("apps thread not in state");
+    //   return;
+    // }
+
+    // this.posts = await this.thread.getPosts();
+    // console.log("posts", this.posts);
+    // await this.thread.onUpdate(async () => {
+    //   this.posts = await this.thread.getPosts();
+    //   console.log("posts", this.posts);
+    // })
   }
 
 }
