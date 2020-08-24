0933141d60b392c621feec22ee907c851ca6abbc
dan13ram <dan13ram@gmail.com>
2020-08-24 20:48:32 +0530

    first version working for grants creation

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,7 +1,32 @@
-import { BigInt, BigDecimal, log } from '@graphprotocol/graph-ts'
+import {
+    BigInt,
+    Bytes,
+    Address,
+} from "@graphprotocol/graph-ts";
 
+import { UnmanagedStream } from "../generated/templates/UnmanagedStream/UnmanagedStream";
 
-export function BigIntEth(): BigInt {
-    let halfEthBigInt = BigInt.fromI32(1000000000)
-    return halfEthBigInt.times(halfEthBigInt)
+class GrantObject {
+    uri: Bytes;
+    totalFunded: BigInt;
+}
+
+export function fetchGrantInfo(
+    address: Address
+): GrantObject | null {
+    let grantInstance = UnmanagedStream.bind(address);
+    let grantObject = new GrantObject();
+
+    let uri = grantInstance.try_getUri();
+    let totalFunding = grantInstance.try_getTotalFunding();
+
+    if (!totalFunding.reverted) {
+        grantObject.totalFunded = totalFunding.value;
+    }
+
+    if (!uri.reverted) {
+        grantObject.uri = uri.value;
+    }
+
+    return grantObject;
 }
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,121 +1,60 @@
-import { Bytes, Address, BigInt } from "@graphprotocol/graph-ts";
+import { Bytes } from "@graphprotocol/graph-ts";
 import {
-  ManagedCappedGrant,
-  LogFundingComplete,
-  LogGrantCancellation,
-  LogFunding,
-  LogRefund,
-  LogPayment,
-  LogPaymentApproval,
-  LogRefundApproval
-} from "../generated/ManagedCappedGrant/ManagedCappedGrant"
+    LogFunding,
+    LogPayment
+} from "../generated/templates/UnmanagedStream/UnmanagedStream";
 
-import { LogNewGrant } from "../generated/GrantFactory/GrantFactory"
-import { returnGrantsInfo } from "./getters"
-import { Fund, Payment, Contract } from "../generated/schema"
-import { ethers } from 'ethers';
-
-// import * as moment from 'moment';
+import { LogNewGrant } from "../generated/UnmanagedStreamFactory/UnmanagedStreamFactory";
+import { fetchGrantInfo } from "./helpers";
+import { Fund, Payment, Grant } from "../generated/schema";
 
 export function handleLogNewGrant(event: LogNewGrant): void {
-  let id = event.params.grant.toHexString()
+    let id = event.params.grant.toHexString();
 
+    let grant = new Grant(id);
+    grant.factoryAddress = event.address;
+    grant.createBy = event.transaction.from;
+    grant.grantId = event.params.id;
+    grant.grantAddress = event.params.grant;
+    grant.grantees = event.params.grantees as Array<Bytes>;
+    grant.amounts = event.params.amounts;
 
-  let contract = new Contract(id);
-  contract.contractAddress = event.address
-  contract.grantId = event.params.id
-  contract.grantAddress = event.params.grant
+    let fetchedGrant = fetchGrantInfo(event.params.grant);
+    grant.totalFunded = fetchedGrant.totalFunded;
+    grant.uri = fetchedGrant.uri;
 
-  const ABI = [{ "anonymous": false, "inputs": [{ "indexed": true, "name": "id", "type": "uint256" }, { "indexed": false, "name": "grant", "type": "address" }], "name": "LogNewGrant", "type": "event" }, { "constant": false, "inputs": [{ "name": "_grantees", "type": "address[]" }, { "name": "_amounts", "type": "uint256[]" }, { "name": "_manager", "type": "address" }, { "name": "_currency", "type": "address" }, { "name": "_targetFunding", "type": "uint256" }, { "name": "_fundingExpiration", "type": "uint256" }, { "name": "_contractExpiration", "type": "uint256" }, { "name": "_extraData", "type": "bytes" }], "name": "create", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }]
-  const iface = new ethers.utils.Interface(ABI);
-  const input = iface.parseTransaction({ data: event.transaction.input })
+    grant.funds = new Array<string>();
+    grant.payments = new Array<string>();
 
-  contract.createBy = event.transaction.from;;
-  contract.grantees = input.args[0];
-  contract.amounts = input.args[1].map((amount) => {
-    return ethers.utils.formatEther(amount);
-  });
-  contract.manager = input.args[2];
-  contract.currency = input.args[3];
-  contract.fundingDeadline = input.args[5].toString(16).toUpperCase();
-  contract.contractExpiration = input.args[6].toString(16).toUpperCase();
-  contract.uri = input.args[7]
-
-  let returnedGrant = returnGrantsInfo(event.params.grant);
-
-  contract.targetFunding = returnedGrant.targetFunding;
-  contract.totalFunding = returnedGrant.totalFunding;
-  contract.totalPayed = returnedGrant.totalPayed;
-  contract.availableBalance = returnedGrant.availableBalance;
-  contract.canFund = returnedGrant.canFund;
-  contract.grantCancelled = returnedGrant.grantCancelled;
-
-  contract.save();
-}
-
-export function handleLogFundingComplete(event: LogFundingComplete): void { }
-
-export function handleLogGrantCancellation(event: LogGrantCancellation): void {
-  let contract = Contract.load(event.address.toHexString());
-
-  if (contract != null) {
-    let returnedGrant = returnGrantsInfo(event.address);
-
-    contract.grantCancelled = returnedGrant.grantCancelled;
-    contract.canFund = returnedGrant.canFund;
-
-    contract.save();
-  }
+    grant.save();
 }
 
 export function handleLogFunding(event: LogFunding): void {
-  let fund = new Fund(event.transaction.hash.toHex());
-  fund.grantAddress = event.address
-  fund.donor = event.params.donor
-  fund.amount = event.params.value
-  fund.save()
+    let fund = new Fund(event.transaction.hash.toHex());
+    fund.grantAddress = event.address;
+    fund.donor = event.params.donor;
+    fund.amount = event.params.value;
+    fund.save();
 
-  // let contract = Contract.load(event.address.toHexString());
-
-  // if (contract != null) {
-  //   let returnedGrant = returnGrantsInfo(event.address);
-
-  //   contract.totalFunding = returnedGrant.totalFunding;
-  //   contract.availableBalance = returnedGrant.availableBalance;
-  //   contract.canFund = returnedGrant.canFund;
-
-  //   if (!contract.donors.find((donor) => donor == event.params.donor)) {
-  //     contract.donors.push(event.params.donor);
-  //   }
-
-  // contract.save();
-  // }
-
+    let grant = Grant.load(event.address.toHexString());
+    if (grant !== null) {
+        let fetchedGrant = fetchGrantInfo(event.address);
+        grant.totalFunded = fetchedGrant.totalFunded;
+        grant.funds.push(fund.id);
+        grant.save();
+    }
 }
-
-export function handleLogRefund(event: LogRefund): void { }
 
 export function handleLogPayment(event: LogPayment): void {
-  let payment = new Payment(event.transaction.hash.toHex());
-  payment.grantee = event.params.grantee
-  payment.grantAddress = event.address
-  payment.amount = event.params.value
-  payment.save();
+    let payment = new Payment(event.transaction.hash.toHex());
+    payment.grantee = event.params.grantee;
+    payment.grantAddress = event.address;
+    payment.amount = event.params.value;
+    payment.save();
 
-
-  let contract = Contract.load(event.address.toHexString());
-
-  if (contract != null) {
-    let returnedGrant = returnGrantsInfo(event.address);
-
-    contract.totalPayed = returnedGrant.totalPayed;
-    contract.availableBalance = returnedGrant.availableBalance;
-
-    contract.save();
-  }
-
+    let grant = Grant.load(event.address.toHexString());
+    if (grant !== null) {
+        grant.payments.push(payment.id);
+        grant.save();
+    }
 }
-
-export function handleLogPaymentApproval(event: LogPaymentApproval): void { }
-
-export function handleLogRefundApproval(event: LogRefundApproval): void { }
