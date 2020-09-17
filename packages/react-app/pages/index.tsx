7521d622cf5b96242b41bb47546b580f9b8859a7
dan13ram <dan13ram@gmail.com>
2020-09-17 23:51:49 +0530

    revalidate fix

--- packages/react-app/pages/index.tsx
+++ packages/react-app/pages/index.tsx
@@ -16,7 +16,7 @@ export const getStaticProps = async () => {
   return {
     props: {
       grants: grants.filter(grant => grant.name !== ''),
-      revalidate: true,
+      revalidate: 1,
     },
   };
 };
