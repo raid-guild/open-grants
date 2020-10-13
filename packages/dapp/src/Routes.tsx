0ce49a225671462af948d0bd6a3c5c2e566d43b2
dan13ram <dan13ram@gmail.com>
2020-10-13 22:40:43 +0530

    profile pages for all users

--- packages/dapp/src/Routes.tsx
+++ packages/dapp/src/Routes.tsx
@@ -2,8 +2,8 @@ import { Web3Context } from 'contexts/Web3Context';
 import Create from 'pages/create';
 import Explore from 'pages/explore';
 import FAQ from 'pages/faq';
-import GrantDetails from 'pages/grant/details';
-import Profile from 'pages/profile';
+import Grant from 'pages/grant/address';
+import Profile from 'pages/profile/address';
 import React, { useContext } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 
@@ -13,9 +13,9 @@ export const Routes: React.FC = () => {
     <Switch>
       <Route path="/explore" component={Explore} />
       {account && <Route path="/create" component={Create} />}
-      {account && <Route path="/profile" component={Profile} />}
+      <Route path="/profile/:userAddress" component={Profile} />
       <Route path="/faq" component={FAQ} />
-      <Route path="/grant/:grantAddress" component={GrantDetails} />
+      <Route path="/grant/:grantAddress" component={Grant} />
       <Redirect to="/explore" />
     </Switch>
   );
--- packages/dapp/src/pages/profile/address.tsx
+++ packages/dapp/src/pages/profile/address.tsx
@@ -4,18 +4,30 @@ import { ProfileHeader } from 'components/ProfileHeader';
 import { Web3Context } from 'contexts/Web3Context';
 import { getProfile } from 'graphql/getProfile';
 import React, { useContext, useEffect, useState } from 'react';
+import { RouteComponentProps } from 'react-router-dom';
 import { Profile } from 'utils/types';
 
-const ProfilePage: React.FC = () => {
+interface MatchParams {
+  userAddress: string;
+}
+
+type Props = RouteComponentProps<MatchParams>;
+
+const ProfilePage: React.FC<Props> = ({
+  match: {
+    params: { userAddress },
+  },
+}) => {
   const { account } = useContext(Web3Context);
+  const loggedInUser = account.toLowerCase() === userAddress.toLowerCase();
   const [profile, setProfile] = useState<Profile | null | undefined>();
 
   useEffect(() => {
     async function fetchProfile() {
-      setProfile(await getProfile(account.toLowerCase()));
+      setProfile(await getProfile(userAddress.toLowerCase()));
     }
     fetchProfile();
-  }, [account]);
+  }, [userAddress]);
 
   if (profile === undefined) {
     return <Box mt="5rem"> Loading ... </Box>;
@@ -25,8 +37,8 @@ const ProfilePage: React.FC = () => {
   }
   return (
     <VStack w="100%">
-      <ProfileHeader profile={profile} />
-      <ProfileContent profile={profile} />
+      <ProfileHeader profile={profile} loggedInUser={loggedInUser} />
+      <ProfileContent profile={profile} loggedInUser={loggedInUser} />
     </VStack>
   );
 };
