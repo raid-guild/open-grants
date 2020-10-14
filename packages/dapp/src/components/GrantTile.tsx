1df40aadca42ecf138646460fef23f4293340d05
jaclynlenee <jaclynlenee@gmail.com>
2020-10-14 16:10:29 -0700

    Nav image compressed

--- packages/dapp/src/components/GrantTile.tsx
+++ packages/dapp/src/components/GrantTile.tsx
@@ -31,6 +31,7 @@ export const GrantTile: React.FC<Props> = ({ grant }) => {
       rounded="lg"
       p={4}
       background="white"
+      boxShadow="0px 4px 4px rgba(114, 125, 129, 0.25)"
       color="gray.500"
       align="stretch"
       bgImage={`url(${TileBG})`}
