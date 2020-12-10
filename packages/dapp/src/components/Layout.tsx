c4643af68cae03dd3b84df65fec08ad44d674811
dan13ram <dan13ram@gmail.com>
2020-12-10 16:51:03 +0530

    network not supported error

--- packages/dapp/src/components/Layout.tsx
+++ packages/dapp/src/components/Layout.tsx
@@ -1,11 +1,68 @@
-import { Flex, useDisclosure } from '@chakra-ui/core';
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  Flex,
+  useDisclosure,
+} from '@chakra-ui/core';
+import HeaderBG from 'assets/header.jpg';
 import { Header } from 'components/Header';
 import { NavBar } from 'components/NavBar';
+import { CONFIG } from 'config';
 import { SearchContextProvider } from 'contexts/SearchContext';
-import React from 'react';
+import { Web3Context } from 'contexts/Web3Context';
+import React, { useContext } from 'react';
 
 export const Layout: React.FC = ({ children }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const { isSupportedNetwork } = useContext(Web3Context);
+  if (!isSupportedNetwork) {
+    return (
+      <Flex
+        w="100%"
+        h="100vh"
+        justify="center"
+        align="center"
+        bgImage={`url(${HeaderBG})`}
+        bgSize="cover"
+        bgRepeat="no-repeat"
+        position="fixed"
+        top={0}
+        left={0}
+        color="white"
+        direction="column"
+        p={4}
+      >
+        <Alert
+          status="error"
+          variant="subtle"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+          textAlign="center"
+          height="200px"
+          bg="transparent"
+        >
+          <AlertIcon
+            boxSize={{ base: '32px', sm: '48px', md: '64px' }}
+            mr={0}
+          />
+          <AlertTitle
+            mt={4}
+            mb={1}
+            fontSize={{ base: '1.5rem', sm: '2rem', md: '3rem' }}
+          >
+            Unsupported Network!
+          </AlertTitle>
+          <AlertDescription maxWidth="sm">
+            Please connect your wallet to {CONFIG.network.name}
+          </AlertDescription>
+        </Alert>
+      </Flex>
+    );
+  }
+
   return (
     <Flex
       p={0}
